3536a2cebbf311f7cc009ac37bd80165b20628ba
Ruby Simpleman <rubysimpleman@gmail.com>
2022-09-21 23:46:29 +0300

    2022-09-21_23-46-29_init

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is a Serghey Vheeshnhevskeey cv-project
---
+++ js/index.mjs
@@ -0,0 +1,5 @@
+// import svgHeaderAutorizeIcon from'../assets/icons/svg-header-autorize-icon.mjs'
+// import { data } from'../data/data.mjs'
+
+// const headerAutorizeIcon = document.querySelector('.header-autorize-icon')
+// headerAutorizeIcon.append(svgHeaderAutorizeIcon)
